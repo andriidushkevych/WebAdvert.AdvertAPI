65ebc30c7a5d9185a93ce0e63659e39d70c902a8
Andrii Dushkevych <adushkevych@logisense.com>
2020-02-27 14:27:11 -0500

    add API models and some service methods

---
+++ AdvertAPI/Services/DynamoDBAdvertsStorage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AutoMapper;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace AdvertAPI.Services
+{
+    public class DynamoDBAdvertsStorage: IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+
+        public DynamoDBAdvertsStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<string> Add(AdvertModel model)
+        {
+            var advertDbModel = _mapper.Map<AdvertDbModel>(model);
+            advertDbModel.Id = new Guid().ToString();
+            advertDbModel.CreationDateTime = DateTime.UtcNow;
+            advertDbModel.Status = AdvertStatus.Pending;
+
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(advertDbModel);
+                }
+            }
+
+            return advertDbModel.Id;
+        }
+
+        public async Task Confirm(ConfirmAdvertModel model)
+        {
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    var record = await context.LoadAsync<AdvertDbModel>(model.Id);
+                    if (record == null) throw new KeyNotFoundException();
+                    if (model.Status == AdvertStatus.Active)
+                    {
+                        record.Status = AdvertStatus.Active;
+                        await context.SaveAsync(record);
+                    }
+                    else
+                    {
+                        await context.DeleteAsync(record);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AdvertApi.Models/AdvertStatus.cs
@@ -0,0 +1,4 @@
+﻿namespace AdvertApi.Models
+{
+    public enum AdvertStatus { Pending = 1, Active = 2 }
+}
