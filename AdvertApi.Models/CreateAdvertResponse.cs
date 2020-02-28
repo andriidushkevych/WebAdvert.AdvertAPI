b87840c18de89e32121bbbf3badfd09a580390d0
Andrii Dushkevych <adushkevych@logisense.com>
2020-02-27 21:51:47 -0500

    Add nuget package publish properties and CreateAdvertResponse

---
+++ AdvertApi.Models/CreateAdvertResponse.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AdvertApi.Models
+{
+    public class CreateAdvertResponse
+    {
+        public string Id { get; set; }
+
+        public CreateAdvertResponse()
+        {
+            
+        }
+
+        public CreateAdvertResponse(string id)
+        {
+            Id = id;
+        }
+    }
+}
